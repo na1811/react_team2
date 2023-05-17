5c81cbd4527c8be80d45cad4f1b3e91d46fe4bd2
NguyenKhaiHoan <banghoan1511@gmail.com>
2023-05-17 19:36:48 +0700

    them ham kiem tra cho update

--- src/components/table.js
+++ src/components/table.js
@@ -7,6 +7,52 @@ function Table() {
   const [uname, usetName] = useState("");
   const [udob, usetDob] = useState("");
   const [uhometown, usetHometown] = useState("");
+  const [errors, setErrors] = useState({});
+
+  function checkValidDob({ udob }) {
+    const format = /^\d{2}\/\d{2}\/\d{4}$/;
+    if (!format.test(udob)) {
+      console.log("lỗi 1");
+      return false;
+    }
+    const [ngay, thang, nam] = udob.split("/").map(Number);
+
+    if (isNaN(ngay) || isNaN(thang) || isNaN(nam)) {
+      console.log("lỗi 2");
+      return false;
+    }
+    if (thang > 12 || thang < 1) {
+      return false;
+    }
+    if (
+      thang === 1 ||
+      thang === 3 ||
+      thang === 5 ||
+      thang === 7 ||
+      thang === 8 ||
+      thang === 10 ||
+      thang === 12
+    ) {
+      if (ngay > 31) return false;
+    } else if (thang === 2) {
+      if ((nam % 4 === 0 && nam % 100 !== 0) || nam % 400 === 0) {
+        if (ngay > 29) return false;
+      } else if (ngay > 28) return false;
+    } else if (ngay > 30) return false;
+
+    if (ngay < 1) return false;
+
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1;
+    if (
+      nam > currentYear ||
+      (thang > currentMonth && nam >= currentYear) ||
+      nam < 1950
+    )
+      return false;
+
+    return true;
+  }
 
   useEffect(() => {
     axios
@@ -29,19 +75,86 @@ function Table() {
   };
 
   const handleUpdate = () => {
-    axios
-      .put("http://localhost:3000/users/" + editId, {
-        id: editId,
-        name: uname,
-        dob: udob,
-        hometown: uhometown,
-      })
-      .then((res) => {
-        console.log(res);
-        window.location.reload();
-        setEdit(-1);
-      })
-      .catch((err) => console.log(err));
+    // Sai
+    if (!uname) {
+      setErrors((prev) => ({
+        ...prev,
+        name: "Họ và tên không được để trống!",
+      }));
+    }
+    if (data.filter((sv) => sv.id !== editId).find((sv) => sv.name === uname)) {
+      setErrors((prev) => ({ ...prev, name: "Họ tên đã tồn tại!" }));
+    }
+    if (uname.length > 100) {
+      setErrors((prev) => ({
+        ...prev,
+        name: "Họ tên có độ dài quá 100 ký tự!",
+      }));
+    }
+    if (data.filter((sv) => sv.id !== editId).find((sv) => sv.hometown === uhometown)) {
+      setErrors((prev) => ({ ...prev, hometown: "Quê quán đã tồn tại!" }));
+    }
+    if (!uhometown) {
+      setErrors((prev) => ({
+        ...prev,
+        hometown: "Quê quán không được để trống!",
+      }));
+    }
+    if (uhometown.length > 100) {
+      setErrors((prev) => ({
+        ...prev,
+        hometown: "Quê quán có độ dài quá 100 ký tự!",
+      }));
+    }
+    if (!checkValidDob(udob)) {
+      setErrors((prev) => ({ ...prev, dob: "Ngày sinh không hợp lệ!" }));
+    }
+    if (!udob) {
+      setErrors((prev) => ({ ...prev, dob: "Ngày sinh không được để trống!" }));
+    }
+
+    // Đúng
+    if (
+      uname.length > 0 &&
+      uname.length <= 100 &&
+      !data.filter((sv) => sv.id !== editId).find((sv) => sv.name === uname)
+    ) {
+      setErrors((prev) => ({ ...prev, name: "" }));
+    }
+    if (
+      uhometown.length > 0 &&
+      uhometown.length <= 100 &&
+      !data.filter((sv) => sv.id !== editId).find((sv) => sv.hometown === uhometown)
+    ) {
+      setErrors((prev) => ({ ...prev, hometown: "" }));
+    }
+    if (checkValidDob({ udob })) {
+      setErrors((prev) => ({ ...prev, dob: "" }));
+    }
+
+    if (
+      uname.length > 0 &&
+      uname.length <= 100 &&
+      !data.filter((sv) => sv.id !== editId).find((sv) => sv.name === uname) &&
+      uhometown.length > 0 &&
+      uhometown.length <= 100 &&
+      !data.filter((sv) => sv.id !== editId).find((sv) => sv.hometown === uhometown) &&
+      checkValidDob({ udob })
+    ) {
+      axios
+        .put("http://localhost:3000/users/" + editId, {
+          id: editId,
+          name: uname,
+          dob: udob,
+          hometown: uhometown,
+        })
+        .then((res) => {
+          console.log(res);
+          window.location.reload();
+          setEdit(-1);
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   const handleDelete = (id) => {
@@ -78,8 +191,16 @@ function Table() {
                   <input
                     type="text"
                     value={uname}
-                    onChange={(e) => usetName(e.target.value)}
+                    onChange={(e) => usetName(e.target.value
+                      .split(" ")
+                      .map(
+                        (word) =>
+                          word.charAt(0).toUpperCase() + word.slice(1)
+                      )
+                      .join(" "))}
                   />
+                  <br />
+                  {errors.name && <span className="error">{errors.name}</span>}
                 </td>
                 <td>
                   <input
@@ -87,13 +208,23 @@ function Table() {
                     value={udob}
                     onChange={(e) => usetDob(e.target.value)}
                   />
+                  <br />
+                  {errors.dob && <span className="error">{errors.dob}</span>}
                 </td>
                 <td>
                   <input
                     type="text"
                     value={uhometown}
-                    onChange={(e) => usetHometown(e.target.value)}
+                    onChange={(e) => usetHometown(e.target.value
+                      .split(" ")
+                      .map(
+                        (word) =>
+                          word.charAt(0).toUpperCase() + word.slice(1)
+                      )
+                      .join(" "))}
                   />
+                  <br />
+                  {errors.hometown && <span className="error">{errors.hometown}</span>}
                 </td>
                 <td>
                   <button
